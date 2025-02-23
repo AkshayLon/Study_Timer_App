4a0eca744217c100a4f55710be424a07b80496a5
Akshay Lonkar <akshaylonkar2004@gmail.com>
2025-02-23 20:08:06 +0000

    revert to hello world

--- optim_logic.py
+++ optim_logic.py
@@ -2,10 +2,9 @@ import numpy as np
 
 class BaysianBelief:
 
-    def __init__(self, wake:float, sleep:float, attention_peak:float):
-        self.avg_wake_time = wake
-        self.avg_sleep_time = sleep
-        self.peak_attention = attention_peak
+    def __init__(self):
+        pass
+  
 
 if __name__=="__main__":
-    print("Now I am changing the IDE!")
+    print("Hello World!")
