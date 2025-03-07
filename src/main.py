039a0a9f3790cff725d7851071c1c844f299bfce
Akshay Lonkar <akshaylonkar2004@gmail.com>
2025-03-07 00:11:05 +0000

    basic timer functions done

--- src/main.py
+++ src/main.py
@@ -2,3 +2,4 @@ from timer import TimerInterface
 
 if __name__=="__main__":
     current_timer = TimerInterface("Study Timer")
+    current_timer.run_interface(work_time=10)
--- src/timer.py
+++ src/timer.py
@@ -5,3 +5,31 @@ class TimerInterface:
 
     def __init__(self, header_name):
         st.title(header_name)
+        self.time_display_placeholder = st.empty()
+        if 'timer_running' not in st.session_state:
+            st.session_state.timer_running = False
+        if 'time_elapsed' not in st.session_state:
+            st.session_state.time_elapsed = 0
+
+    def _nice_display_time(self, total_seconds):
+        hours = int(total_seconds/3600)
+        mins = int((total_seconds%3600)/60)
+        secs = total_seconds%60
+        return f"{hours:02}:{mins:02}:{secs:02}"
+
+    def run_interface(self, work_time):
+        
+        if st.button("Start Timer"):
+            st.session_state.time_elapsed = 0
+            st.session_state.timer_running = True
+
+        if st.button("Stop Focus Session"):
+            st.session_state.timer_running = False
+
+        time_display = st.empty()
+
+        while st.session_state.timer_running:
+            time_display.write(self._nice_display_time(abs(work_time-st.session_state.time_elapsed)))
+            time.sleep(1)
+            st.session_state.time_elapsed += 1
+            st.rerun()
