85379261919bc4698fb190a6c0c75d64edc0cd44
Akshay Lonkar <akshaylonkar2004@gmail.com>
2025-03-17 03:52:09 +0000

    removed redundant debug files

--- src/main.py
+++ src/main.py
@@ -4,4 +4,4 @@ from session_manager import SessionHandler
 if __name__=="__main__":
     data_object = SessionHandler("..\\data\\params.json", "..\\data\\sessions.db")
     current_timer = TimerInterface("Study Timer", data_object)
-    current_timer.run_interface(work_time=10)
+    current_timer.run_interface(work_time=3300)
